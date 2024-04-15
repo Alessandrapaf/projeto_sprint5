052d17d7cf3408b7da2244c6598c47bba7d0a428
Alessandra Fonseca <lelefonseca18@gmail.com>
2024-04-15 01:11:08 -0300

    criando a caixa de seleção e gráficos

---
+++ app.py
@@ -0,0 +1,36 @@
+# importando as bibliotecas
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('../notebooks/vehicles.csv')  # lendo os dados
+
+st.header('Anúncios de vendas de carros')  # cabeçalho
+
+
+# Caixa de seleção para escolher o tipo de gráfico
+build_histogram = st.checkbox('Criar um histograma')
+build_scatterplot = st.checkbox('Criar um gráfico de dispersão')
+
+# Se a caixa de seleção para histograma estiver selecionada
+if build_histogram:
+    # Escrever mensagem
+    st.write('Criando um histograma para a coluna odometer')
+
+    # Criar histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # Exibir o gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+
+# Se a caixa de seleção para gráfico de dispersão estiver selecionada
+if build_scatterplot:
+    # Escrever mensagem
+    st.write(
+        'Criando um gráfico de dispersão para as colunas odometer e price')
+
+    # Criar gráfico de dispersão
+    fig = px.scatter(car_data, x='odometer', y='price')
+
+    # Exibir o gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
